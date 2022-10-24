99f19c92c6b1c324a3cb1dd1918acbe327f3a64a
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2022-10-24 13:59:06 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -23,6 +23,16 @@ def game_1on1():
     print('Все конфеты оппонента достаются сделавшему последний ход')
     rand_opponent = random.randint(1, 2)
     print(f'Первый ход делает игрок №{rand_opponent}')
+   
+
+def check_win ():
+    flag = False
+    if num == 0:
+        print(' Вы Выиграли, поздравляем!!!' )
+        flag = True
+        return flag
+        
+         
 
 game_1on1()
 # def game (rand_opponent):
@@ -39,6 +49,15 @@ while num > 1:
     if player1 > 28 or player1 < 1:
         while player1 > 28 or player1 < 1:
             player1 = int(input('Ходит первый игрок, введите ПРАВИЛЬНОЕ число от 1 до 28 :'))
+    check_win()
+    if flag == True:
+        num  = 100
+        player1 = 0
+        total_player1 = 0
+
+        player2 = 0
+        total_player2 = 0
+        print(f' Все конфеты достаются Player1  а это целых {num} конфет. Поздравляю!!!')
     num -= player1
     total_player1 += player1
     print(f'На столе конфет {num}, у первого игрока {total_player1} конфет, у второго игрока {total_player2} конфет')
@@ -46,6 +65,17 @@ while num > 1:
     if player2 > 28 or player2 < 1:
         while player2 > 28 or player2 < 1:
             player2 = int(input('Ходит ВТОРОЙ игрок, введите ПРАВИЛЬНОЕ число от 1 до 28 :'))
+    check_win()
+    if flag == True:
+        num  = 100
+        player1 = 0
+        total_player1 = 0
+
+        player2 = 0
+        total_player2 = 0
+        print(f' Все конфеты достаются Player2  а это целых {num} конфет. Поздравляю!!!')
+    num -= player1
+    total_player1 += player1
     num -= player2
     total_player2 += player2
     
